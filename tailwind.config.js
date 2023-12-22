922dfb0223812a855b60246190e59ab35df0e2d7
Leroy Mogire <ntruderleroy@gmail.com>
2023-12-22 21:19:42 +0300

    tailwind first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        screens: {
+            sm: "500px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        extend: {
+            fontFamily: {
+                sans: ["Josefin Sans", "sans-serif"],
+                alata: ["alata"],
+            },
+        },
+        colors: {
+            veryDarkGrey: "hsl(0, 0%, 41%)",
+            darkGrey: "hsl(0, 0%, 55%)",
+            white: "#fff",
+            black: "#000",
+        },
+    },
+    plugins: [],
+};
